3fe95f2ea3b0a0ce74c21a0af8e62bbb6bc0735b
twankruiswijk <twankrui@gmail.com>
2025-01-26 14:27:47 +0100

    init v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
+// @ts-check
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+	integrations: [react(), tailwind()],
 });

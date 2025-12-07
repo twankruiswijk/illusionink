f7b6fecb6fd0f2ccf0d8881925c2ebc32513b5f1
twankruiswijk <twankrui@gmail.com>
2025-12-07 15:52:47 +0100

    chore: remove astro adapter to go full static rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,9 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-      plugins: [tailwindcss()],
+	integrations: [react()],
+	vite: {
+		plugins: [tailwindcss()],
 	},
-
-  adapter: cloudflare(),
 });
